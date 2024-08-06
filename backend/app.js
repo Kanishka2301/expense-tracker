899611e5e95b9d47b03bfecde7965d6a5a5451c5
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-08-06 15:12:07 +0530

    app

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,23 @@
+const { db } = require("./db/db");
 const express = require("express");
 const cors = require("cors");
+const { readdirSync } = require("fs");
 
 const app = express();
 
 require("dotenv").config();
+console.log("MongoDB URL:", process.env.MONGODB_URL); // Ensure this prints the correct URL
 
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
 
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
 const server = () => {
+  db();
   app.listen(PORT, () => {
     console.log("listening to port:", PORT);
   });
--- backend/db/db.js
+++ backend/db/db.js
@@ -1,1 +1,14 @@
 const mongoose = require("mongoose");
+
+const db = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("DB COnnected");
+  } catch (error) {
+    console.error("DB Connection Error:", error.message);
+    console.error("Full Error Details:", error);
+  }
+};
+
+module.exports = { db };
